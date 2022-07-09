f8695400e2ee40f127e85b31fff887ce7597ad34
IsabelaMarques07 <dev.isabelamarques@gmail.com>
2022-07-08 22:10:03 -0300

    adicionando reactstrap ao projeto

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
-
+import React from 'react'
+import Head from 'next/head'
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Head>
+      <title>Create Next App</title>
+      <link rel="stylesheet" 
+        href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+    )
 }
-
 export default MyApp
